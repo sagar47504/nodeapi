f1c0876b96a827aac7d013b04189af8523c02f2c
sagar <sagar47504@gmail.com>
2025-03-30 00:56:46 +0530

    remove pm2

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 2000;
 const indexRouter = require('./routes/index');
 
 app.use(express.json());
@@ -8,8 +8,12 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/', indexRouter);
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+app.listen(port, (error) =>{
+  if(!error)
+      console.log("Server is Successfully Running, and App is listening on port "+ port);
+  else 
+      console.log("Error occurred, server can't start", error);
+  }
+);
 
 module.exports = app;
